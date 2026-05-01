fa88dc194cf9025c45ba662d6e8407c794d09345
dilsontorrefiel <cpe.dilsontorrefiel@gmail.com>
2026-05-01 16:36:24 +0400

    added cv file and js handling

---
+++ script/pdf.js
@@ -0,0 +1,9 @@
+/**
+ * This function opens the PDF file in a new browser tab when called.
+ * It uses the window.open() method to open the specified PDF URL.
+ * Make sure to replace "./files/my-cv.pdf" with the actual path to your PDF file.
+ */
+function openPDF() {
+  const pdfUrl = "./files/my-cv.pdf";
+  window.open(pdfUrl, "_blank");
+}
